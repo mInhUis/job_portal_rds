7518c67db0ab8101398fe73a1f394bdde9e52a4d
cOOlFish <nonexistent@example.com>
2026-06-08 21:22:02 +0700

    test backned cicd pipeline

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/applications', applyRoutes);
 app.get('/api/ping', (req, res) => {
-    res.json({ message: 'pingccc ccc  cc con cac tuoi lon scc cc a cc' });
+    res.json({ message: 'pingccc cc cc ccc  cc con cac tuoi lon scc cc a cc' });
 });
 /* git add .
 git commit -m "test backned cicd pipeline"
