4283fef7d93363e6043320c137bb9abcce203bfd
cOOlFish <nonexistent@example.com>
2026-06-09 01:14:01 +0700

    test backned cicd pipeline

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = createPool({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     
     waitForConnections: true,
     connectionLimit: 10
